3cd11401a10cd78380f08907498a51013b6ff738
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2022-01-06 10:21:41 +0100

    moved pixelate_roi and get_autorange to helpers module

--- yarppg/ui/helpers.py
+++ yarppg/ui/helpers.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pyqtgraph as pg
+import cv2
 
 
-def add_plot(p1, **kwargs):
+def add_multiaxis_plot(p1, **kwargs):
     p2 = pg.ViewBox()
     p1.scene().addItem(p2)
     p1.hideAxis("right")
@@ -18,3 +20,25 @@ def add_plot(p1, **kwargs):
     p1.vb.sigResized.connect(update_view)
 
     return line, p2
+
+
+def pixelate_roi(img, roi, blursize):
+    if blursize > 0:
+        roiw, roih = roi[2] - roi[0], roi[3] - roi[1]
+        if roiw <= blursize or roih <= blursize:
+            return
+        slicey = slice(roi[1], roi[3])
+        slicex = slice(roi[0], roi[2])
+        tmp = cv2.resize(img[slicey, slicex],
+                            (int(roiw/blursize), int(roih/blursize)),
+                            interpolation=cv2.INTER_LINEAR)
+        img[slicey, slicex] = cv2.resize(tmp, (roiw, roih),
+                                            interpolation=cv2.INTER_NEAREST)
+
+
+def get_autorange(data, factor):
+    if np.all(np.isnan(data)):
+        return 0, 1
+    x1, x2 = np.nanmin(data), np.nanmax(data)
+    pad = (x2 - x1) * factor
+    return x1 - pad, x2 + pad
