a754327ed31b2649c2119f9ccd00bac6b4bda9f2
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2022-01-18 20:30:13 +0100

    added pixelation of facerect inside ROI class; added LiCvprProcessor for Li-2014 (CVPR) algorithm
    Former-commit-id: a70ad50197001b8d80438ab175fb72514bdda85b

--- yarppg/rppg/roi/region_of_interest.py
+++ yarppg/rppg/roi/region_of_interest.py
@@ -1,48 +1,64 @@
+import functools
+
+from multiprocessing.sharedctypes import Value
 import cv2
 import numpy as np
 
 
+def pixelate(img, xywh, blur):
+    if blur > 0:
+        x, y, w, h = xywh
+        slicex = slice(x, x+w)
+        slicey = slice(y, y+h)
+
+        tmp = cv2.resize(img[slicey, slicex], (w//blur, h//blur),
+                         interpolation=cv2.INTER_LINEAR)
+        img[slicey, slicex] = cv2.resize(tmp, (w, h),
+                                         interpolation=cv2.INTER_NEAREST)
+
+@functools.lru_cache(maxsize=2)
+def get_default_bgmask(w, h):
+    mask = np.zeros((h, w), dtype="uint8")
+    cv2.rectangle(mask, (0, 0), (w, 5), 255, -1)
+
+    return mask
+
 class RegionOfInterest:
-    def __init__(self, base_img, mask=None):
+    def __init__(self, base_img, mask=None, bgmask=None, facerect=None):
         self.rawimg = base_img
 
         self._mask = mask
-        self._empty = False
-        if mask is None:
-            self._empty = True
-
-        self._rectangular = False
         self._rectangle = None
+        self._empty = True
+        self._rectangular = False
         self._contours = None
-        # TODO: Calc bounding box for non-rectangular masks
+        self._bgmask = bgmask
+        self._facerect = facerect
+
+        if mask is not None:
+            self._rectangle = cv2.boundingRect(mask)
+            self._empty = (self._rectangle[2] == 0 or self._rectangle[3] == 0)
 
     @classmethod
-    def from_rectangle(cls, base_img, p1, p2):
+    def from_rectangle(cls, base_img, p1, p2, **kwargs):
         # https://www.pyimagesearch.com/2021/01/19/image-masking-with-opencv/
         mask = np.zeros(base_img.shape[:2], dtype="uint8")
         cv2.rectangle(mask, p1, p2, 255, cv2.FILLED)
-        roi = RegionOfInterest(base_img, mask=mask)
+
+        roi = RegionOfInterest(base_img, mask=mask, **kwargs)
         roi._rectangular = True
-        x, y, w, h = cv2.boundingRect(mask)
-        roi._rectangle = x, y, w, h
-        if (w+h) > 0:
-            roi._empty = False
 
         return roi
 
     @classmethod
-    def from_contour(cls, base_img, pointlist):
+    def from_contour(cls, base_img, pointlist, **kwargs):
         # pointlist with shape nx2
         mask = np.zeros(base_img.shape[:2], dtype="uint8")
         contours = np.reshape(pointlist, (1, -1, 1, 2))
         cv2.drawContours(mask, contours, 0, color=255, thickness=cv2.FILLED)
-        x, y, w, h = cv2.boundingRect(mask)
 
-        roi = RegionOfInterest(base_img, mask)
-        roi._rectangle = x, y, w, h
+        roi = RegionOfInterest(base_img, mask, **kwargs)
         roi._contours = contours
-        if (w+h) == 0:
-            roi._empty = True
 
         return roi
 
@@ -56,6 +72,11 @@ class RegionOfInterest:
         else:
             cv2.drawContours(img, self._contours, 0, color=color,
                              thickness=thickness)
+
+    def pixelate_face(self, img, blursize):
+        if not self.is_empty():
+            xywh = self._rectangle if self._facerect is None else self._facerect
+            pixelate(img, xywh, blursize)
 
     def is_rectangular(self):
         return self._rectangular
@@ -71,8 +92,14 @@ class RegionOfInterest:
             return (x, y), (x+w, y+h)
         return self._rectangle
 
-    def get_mean_rgb(self):
-        r, g, b, a = cv2.mean(self.rawimg, self._mask)
+    def get_mean_rgb(self, background=False):
+        mask = self._mask
+        if background:
+            if self._bgmask is None:
+                raise ValueError("Background mask is not specified")
+            mask = self._bgmask
+
+        r, g, b, a = cv2.mean(self.rawimg, mask)
         return r, g, b
 
     def __str__(self):
