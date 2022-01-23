caaa1dc2ac7b2c53f6c8c1eefe32fc2eb14d19ef
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2022-01-23 14:10:06 +0100

    moved helpers from main to yarppg.ui.cli

--- yarppg/main.py
+++ yarppg/main.py
@@ -6,74 +6,16 @@ import argparse
 from PyQt5.QtWidgets import QApplication
 from yarppg.ui import MainWindow
 from yarppg.rppg import RPPG
-from yarppg.rppg.roi.roi_detect import CaffeDNNFaceDetector, FaceMeshDetector, HaarCascadeDetector, NoDetector
-from yarppg.rppg.processors import (ColorMeanProcessor, ChromProcessor,
-                                    FilteredProcessor, PosProcessor, LiCvprProcessor)
-from yarppg.rppg.hr import HRCalculator, from_fft
-from yarppg.rppg.filters import DigitalFilter, get_butterworth_filter
-
-
-_mainparser = argparse.ArgumentParser(description="Use your Webcam to measure"
-                                                  "your heart rate")
-_mainparser.add_argument("--detector", default="facemesh", type=str,
-                         choices=["facemesh", "caffe-dnn", "haar", "full"],
-                         help="ROI (face) detector")
-_mainparser.add_argument("--processor", default="LiCvpr",
-                         choices=["LiCvpr", "Pos", "Chrom"],
-                         help=("Processor translating ROI to pulse signal. "
-                               "LiCvpr currently only returns mean green value"))
-_mainparser.add_argument("--winsize", default=32, type=int,
-                         help="Window sized used in some processors")
-_mainparser.add_argument("--bandpass", type=str, default="0.5,2",
-                         help="bandpass frequencies for processor output")
-_mainparser.add_argument("--blobsize", default=150, type=int,
-                         help="quadratic blob size of DNN Face Detector")
-_mainparser.add_argument("--draw-facemark", action="store_true",
-                         help="draw landmarks when using facemesh detector")
-_mainparser.add_argument("--blur", default=-1, type=int,
-                         help="pixelation size of detected ROI")
-_mainparser.add_argument("--video", default=0, type=int,
-                         help="video input device number")
-_mainparser.add_argument("--savepath", default="", type=str,
-                         help="store generated signals as data frame")
-
-
-
-def get_detector(args):
-    name = args.detector.lower()
-
-    if name == "full":
-        return NoDetector()
-    elif name == "facemesh":
-        return FaceMeshDetector(draw_landmarks=args.draw_facemark)
-    elif name == "caffe-dnn":
-        return CaffeDNNFaceDetector(blob_size=args.blobsize)
-    elif name == "haar":
-        return HaarCascadeDetector()
-
-    raise NotImplementedError(f"detector {args.detector!r} not recognized.")
-
-
-def get_processor(args):
-    name = args.processor.lower()
-    if name == "licvpr":
-        return LiCvprProcessor()
-    elif name == "pos":
-        return PosProcessor(winsize=args.winsize)
-    elif name == "chrom":
-        return ChromProcessor(winsize=args.winsize, method="xovery")
-
-
-def parse_frequencies(s):
-    """Rudimentary parser of frequency string expected as 'f1,f2' (e.g. '0.4,2')
-    """
-    if s == "none":
-        return None
-    return list(map(float, s.split(",")))
+from yarppg.rppg.processors import ColorMeanProcessor, FilteredProcessor
+from yarppg.rppg.hr import HRCalculator
+from yarppg.rppg.filters import get_butterworth_filter
+from yarppg.ui.cli import (get_detector, get_mainparser, get_processor,
+                           parse_frequencies)
 
 
 def main():
-    args = _mainparser.parse_args(sys.argv[1:])
+    parser = get_mainparser()
+    args = parser.parse_args(sys.argv[1:])
     app = QApplication(sys.argv)
 
     roi_detector = get_detector(args)
---
+++ yarppg/ui/cli.py
@@ -0,0 +1,66 @@
+import argparse
+
+from yarppg.rppg.roi.roi_detect import (CaffeDNNFaceDetector, FaceMeshDetector,
+                                        HaarCascadeDetector, NoDetector)
+from yarppg.rppg.processors import ChromProcessor, LiCvprProcessor, PosProcessor
+
+
+def get_mainparser():
+    parser = argparse.ArgumentParser(description="Use your Webcam to measure"
+                                     "your heart rate")
+    parser.add_argument("--detector", default="facemesh", type=str,
+                        choices=["facemesh", "caffe-dnn", "haar", "full"],
+                        help="ROI (face) detector")
+    parser.add_argument("--processor", default="LiCvpr",
+                        choices=["LiCvpr", "Pos", "Chrom"],
+                        help=("Processor translating ROI to pulse signal. "
+                              "LiCvpr currently only returns mean green value"))
+    parser.add_argument("--winsize", default=32, type=int,
+                        help="Window sized used in some processors")
+    parser.add_argument("--bandpass", type=str, default="0.5,2",
+                        help="bandpass frequencies for processor output")
+    parser.add_argument("--blobsize", default=150, type=int,
+                        help="quadratic blob size of DNN Face Detector")
+    parser.add_argument("--draw-facemark", action="store_true",
+                        help="draw landmarks when using facemesh detector")
+    parser.add_argument("--blur", default=-1, type=int,
+                        help="pixelation size of detected ROI")
+    parser.add_argument("--video", default=0, type=int,
+                        help="video input device number")
+    parser.add_argument("--savepath", default="", type=str,
+                        help="store generated signals as data frame")
+
+    return parser
+
+
+def get_detector(args):
+    name = args.detector.lower()
+
+    if name == "full":
+        return NoDetector()
+    elif name == "facemesh":
+        return FaceMeshDetector(draw_landmarks=args.draw_facemark)
+    elif name == "caffe-dnn":
+        return CaffeDNNFaceDetector(blob_size=args.blobsize)
+    elif name == "haar":
+        return HaarCascadeDetector()
+
+    raise NotImplementedError(f"detector {args.detector!r} not recognized.")
+
+
+def get_processor(args):
+    name = args.processor.lower()
+    if name == "licvpr":
+        return LiCvprProcessor()
+    elif name == "pos":
+        return PosProcessor(winsize=args.winsize)
+    elif name == "chrom":
+        return ChromProcessor(winsize=args.winsize, method="xovery")
+
+
+def parse_frequencies(s):
+    """Rudimentary parser of frequency string expected as 'f1,f2' (e.g. '0.4,2')
+    """
+    if s == "none":
+        return None
+    return list(map(float, s.split(",")))
