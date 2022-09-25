7d7a5f47603c0946a61df3ff7777aceca724ad64
some_beginner_researcher <70256140+blank-ed@users.noreply.github.com>
2022-09-25 18:51:17 +0800

    Update to LiCVPR roi_detect

--- yarppg/rppg/roi/roi_detect.py
+++ yarppg/rppg/roi/roi_detect.py
@@ -5,6 +5,9 @@ import time
 import cv2
 import numpy as np
 import mediapipe as mp
+from tf_bodypix.api import download_model, load_model, BodyPixModelPaths
+
+bodypix_model = load_model(download_model(BodyPixModelPaths.MOBILENET_FLOAT_50_STRIDE_16))
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
@@ -154,6 +157,14 @@ class FaceMeshDetector(ROIDetector):
 
     def detect(self, frame):
         rawimg = frame.copy()
+        bg_rawimg = frame.copy()
+        
+        result = bodypix_model.predict_single(bg_rawimg) # Body Detections
+        mask = result.get_mask(threshold=0.5).numpy().astype(np.uint8) # Body Detections
+        masked_image = cv2.bitwise_and(bg_rawimg, bg_rawimg, mask=mask)  # Get the foreground image
+        bg_image = cv2.subtract(bg_rawimg, masked_image)  # Get the background image
+        gray_bg_image = cv2.cvtColor(bg_image, cv2.COLOR_BGR2GRAY)  # Convert to gray
+        _, thresh = cv2.threshold(gray_bg_image, 10, 255, cv2.THRESH_BINARY)  # Threshold
 
         frame.flags.writeable = False
         results = self.face_mesh.process(frame)
@@ -171,7 +182,7 @@ class FaceMeshDetector(ROIDetector):
         bgmask = get_default_bgmask(frame.shape[1], frame.shape[0])
 
         return RegionOfInterest.from_contour(rawimg, landmarks[self._lower_face],
-                                             facerect=facerect, bgmask=bgmask)
+                                             facerect=facerect, bgmask=bgmask, thresh=thresh)
 
     def draw_facemesh(self, img, multi_face_landmarks, tesselate=False,
                       contour=False, irises=False):
