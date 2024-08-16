d23e486ec72b24595346acb0b8fb1f7292ae6035
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2024-08-16 17:22:37 +0200

    more documenation and utilities

--- src/yarppg/processors/processor.py
+++ src/yarppg/processors/processor.py
@@ -1,3 +1,5 @@
+"""Base processor for rPPG signal computation."""
+
 from dataclasses import dataclass
 
 import cv2
@@ -8,6 +10,8 @@ from ..roi.region_of_interest import RegionOfInterest
 
 @dataclass
 class Color:
+    """Defines a color in RGB(A) format."""
+
     r: float
     g: float
     b: float
@@ -16,6 +20,15 @@ class Color:
     @classmethod
     def null(cls):
         return cls(np.nan, np.nan, np.nan)
+
+    def __array__(self):
+        return np.array([self.r, self.g, self.b, self.a])
+
+    @classmethod
+    def from_array(cls, arr: np.ndarray):
+        if len(arr) in {3, 4} and arr.ndim == 1:
+            return cls(*arr)
+        raise ValueError(f"Cannot interpret {arr=!r}")
 
 
 def masked_average(frame: np.ndarray, mask: np.ndarray) -> Color:
@@ -26,14 +39,22 @@ def masked_average(frame: np.ndarray, mask: np.ndarray) -> Color:
 
 @dataclass
 class RppgResult:
+    """Container for rPPG computation results."""
+
     value: float
     roi: RegionOfInterest
     roi_mean: Color
     bg_mean: Color
 
+    def __array__(self):
+        return np.asarray([self.value])
+
 
 class Processor:
+    """Default rPPG processor."""
+
     def process(self, frame: np.ndarray, roi: RegionOfInterest):
+        """Calculate average green channel in the roi area."""
         avg = masked_average(frame, roi.mask)
         bg_mean = Color.null()
         if roi.bg_mask is not None:
