07c585bdf30a7b4907effed5b6634ec6bc778a5d
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2024-08-16 14:10:47 +0200

    minor change

--- src/yarppg/processors/processor.py
+++ src/yarppg/processors/processor.py
@@ -6,22 +6,22 @@ import numpy as np
 from ..roi.region_of_interest import RegionOfInterest
 
 
-def calculate_color_means(
-    frame: np.ndarray, mask: np.ndarray
-) -> tuple[float, float, float]:
-    r, g, b, _ = cv2.mean(frame, mask)
-    return r, g, b
-
-
 @dataclass
 class Color:
     r: float
     g: float
     b: float
+    a: float = 1.0
 
     @classmethod
     def null(cls):
         return cls(np.nan, np.nan, np.nan)
+
+
+def masked_average(frame: np.ndarray, mask: np.ndarray) -> Color:
+    """Calculate average color of the masked region."""
+    r, g, b, a = cv2.mean(frame, mask)
+    return Color(r, g, b, a)
 
 
 @dataclass
@@ -34,9 +34,9 @@ class RppgResult:
 
 class Processor:
     def process(self, frame: np.ndarray, roi: RegionOfInterest):
-        r, g, b = calculate_color_means(frame, roi.mask)
-        if roi.bg_mask is None:
-            bg_mean = Color.null()
-        else:
-            bg_mean = Color(*calculate_color_means(frame, roi.bg_mask))
-        return RppgResult(g, roi, roi_mean=Color(r, g, b), bg_mean=bg_mean)
+        avg = masked_average(frame, roi.mask)
+        bg_mean = Color.null()
+        if roi.bg_mask is not None:
+            bg_mean = masked_average(frame, roi.bg_mask)
+
+        return RppgResult(avg.g, roi, roi_mean=avg, bg_mean=bg_mean)
